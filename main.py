4ce45c527c6d41640176d0b587064e40d841e321
s9096309 <k-hoff-mann@web.de>
2025-01-22 14:47:45 +0100

    FUNKTIONIERT.
    code clean-up (removed docstrings)

--- main.py
+++ main.py
@@ -33,29 +33,7 @@ def main():
         print(f"Für {current_location} hast du folgende Musikempfehlung:")
         print(musik_link)
 
-        """
-        TODO suggestion for pseudo code for high level logic:
-        Team T: A, B, C (einmalig)
-
-        while True:
-            neue nachrichten abrufen
-            fuer jede neue Nachricht, extrahiere Standort
-            frage Wetter ab
-            gib Musikempfehlung
-            verschicke SMS (wichtig: Hier ein Timer einbauen, rate limit, etc damit ihr nicht mehr als eine SMS alle 10 Minuten schickt!)
-
-            A: Bin in Muenchen
-            App: In Muenchen ist es sonnig, hoere klassische Musik <spotify playlist link>
-
-            B: Bin in Berlin
-            App: In Berlin regnet es, hoere Pop Musik <spotify playlist link>
-
-            ...
-
-
-        """
-
-        # SMS senden (Beispiel-Nummer)
+        # SMS senden
         phone_number = "4915734483417"
         message = f"Wetterbericht für {current_location}: {temperature}°C, {weather_condition}. Musikempfehlung: {musik_link}"
 
